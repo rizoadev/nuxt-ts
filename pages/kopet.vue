23c5d9863856eeb39eaaf019f78a566c0686cea0
Riza Masykur <riza@ikamai.com>
2020-06-19 22:00:44 +0700

    pindah ke pc

---
+++ pages/kopet.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <KopetCom :user="usr" />
+    <KopetClass :user="usr" />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import KopetCom from '~/components/kopetcom'
+import KopetClass from '~/components/kopetclass'
+
+export default Vue.extend({
+  components: {
+    KopetCom,
+    KopetClass,
+  },
+  data() {
+    return {
+      usr: {
+        firstName: 'paijo',
+        lastName: 'dakkar',
+      },
+    }
+  },
+})
+</script>
